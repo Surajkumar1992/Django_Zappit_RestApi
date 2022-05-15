c82dc1106e0cb61dfbe91f92d9c80f21b4d0cf30
SurajKumar1992 <suraj.subham@gmail.com>
2022-05-15 11:35:43 +0530

    Added Poster and Voter Functionality

--- zappit/settings.py
+++ zappit/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-$bvo@d81nkfdxpiw)@9mdaeyp5yjz)h4yju4f*w0li_%^e=28x'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production!!
 DEBUG = True
 
 ALLOWED_HOSTS = []
